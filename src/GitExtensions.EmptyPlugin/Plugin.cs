b1417762a6d32be1d93baf9cf43d9a7c87a81a4b
Marek Fišera <mara@neptuo.com>
2018-12-26 00:06:20 +0100

    Add empty plugin class.

---
+++ src/GitExtensions.EmptyPlugin/Plugin.cs
@@ -0,0 +1,30 @@
+﻿using GitUIPluginInterfaces;
+using ResourceManager;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitExtensions.BundleBackuper
+{
+    [Export(typeof(IGitPlugin))]
+    public class Plugin : GitPluginBase
+    {
+        public Plugin()
+        {
+            Name = "EmptyPlugin";
+            Description = "Empty Plugin";
+        }
+
+        public override bool Execute(GitUIEventArgs gitUiCommands)
+        {
+            MessageBox.Show("Hello from Empty Plugin.");
+            return true;
+        }
+    }
+}
